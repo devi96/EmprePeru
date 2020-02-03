2af6524e1dea6540f5ba410c556c6c8e2c198e4c
devi96 <harold1a96@gmail.com>
2020-02-03 02:46:22 -0500

    agregando el html y conectando a heroku

--- routes/index.route.js
+++ routes/index.route.js
@@ -3,24 +3,29 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/",(req,res) =>{
-	res.render("login");
+	res.render("index");
 });
 
-router.get("/usuarioEmpresarioDashboard",(req,res)=>{
-	res.render("usuarioEmpresarioDashboard");	
-});
-
-router.get("/usuarioNormalDashboard",(req,res)=>{
-	res.render("usuarioNormalDashboard");	
-});
-
-router.get("/historiaExito",(req,res)=>{
-	res.render("historiaExito");	
+router.get("/index",(req,res) =>{
+	res.render("index");
 });
 
 
-router.get("/registroUsuario",(req,res)=>{
-	res.render("registroUsuario");	
+router.get("/login",(req,res)=>{
+	res.render("login");	
+});
+
+router.get("/historia",(req,res)=>{
+	res.render("historia");	
+});
+
+router.get("/contacto",(req,res)=>{
+	res.render("contacto");	
+});
+
+
+router.get("/blog",(req,res)=>{
+	res.render("blog");	
 });
 
 
