403c680a0fe7ac0b3c5354b047f55261bd301f08
devi96 <harold1a96@gmail.com>
2020-02-06 12:12:03 -0500

    falto esto13

--- controller/historias.controller.js
+++ controller/historias.controller.js
@@ -10,7 +10,7 @@ exports.create = (req, res) => {
 	      message: "Content can not be empty!"
 	    });
 	  }
-	  console.log("este es el mensaje", req.body.about);
+	  console.log("este es el mensaje", JSON.stringify(req.body.about) );
 
 	  const historia = new Historia({
 	  	titulo: req.body.display_name,
